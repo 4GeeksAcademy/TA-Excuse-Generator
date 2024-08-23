dbc1218828f7c4378708ed94c8afbd8a96efdd8b
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-08-23 23:39:39 +0000

    updated to character name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#theName").innerHTML = generateName();
+  });
+};
+
+const generateName = () => {
+  let first = ["Luna", "Amber", "Leo", "Iris", "Asher"];
+  let connectors = [
+    "Van der",
+    "Del",
+    "De",
+    "Von",
+    "O'",
+    "Di",
+    "Mc",
+    "Mac",
+    "La"
+  ];
+  let last = [
+    "Unicorn",
+    "Ice Cream",
+    "Rainbow",
+    "Shooting Stars",
+    "Cotton Candy"
+  ];
+
+  let firstIndex = Math.floor(Math.random() * first.length);
+  let connectorIndex = Math.floor(Math.random() * connectors.length);
+  let lastIndex = Math.floor(Math.random() * last.length);
+
+  let fullName =
+    first[firstIndex] +
+    " " +
+    connectors[connectorIndex] +
+    " " +
+    last[lastIndex];
+  document.querySelector("#theName").innerHTML = fullName;
+
+  return fullName;
 };
